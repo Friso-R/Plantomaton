5e147c6f10d8356f191f1fd279c803be74ba4a9e
Friso-R <45263643+Friso-R@users.noreply.github.com>
2025-08-08 23:17:54 +0200

    LED_CHANNEL for PWM control

--- src/LedPWM.h
+++ src/LedPWM.h
@@ -4,19 +4,21 @@
 class LedPWM{
 
   int LED_PIN;
+  int LED_CHANNEL;
 
 public:
 
   LedPWM(int pin, int channel, int freq = 5000, int resolution = 8){
     LED_PIN = pin;
+    LED_CHANNEL = channel;
     ledcSetup(channel, freq, resolution);
     ledcAttachPin(pin, channel);
 
     ledcWrite(channel, 0);
   }
   
-  void set(int pwm) { analogWrite(LED_PIN,  pwm); Serial.println("LED " + String(LED_PIN) + " ON!" ); }
-  void off()        { analogWrite(LED_PIN,  0);   Serial.println("LED " + String(LED_PIN) + " OFF!"); }
+  void set(int pwm) { ledcWrite(LED_CHANNEL,  pwm); Serial.println("LED " + String(LED_PIN) + " ON!" ); }
+  void off()        { ledcWrite(LED_CHANNEL,  0);   Serial.println("LED " + String(LED_PIN) + " OFF!"); }
 };
 
 #endif
